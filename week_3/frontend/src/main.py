c33191707af6f034978e07e424c94b8e7bdf6431
Vaishnavi <vaishnavi.yudeyeljodi@gmail.com>
2026-05-26 18:32:54 +0800

    feat: create user input and scrollable chat history UI components

--- week_3/frontend/src/main.py
+++ week_3/frontend/src/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+# from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# app.mount("/static", StaticFiles(directory="src/static"), name="static")
 templates = Jinja2Templates(directory="src/templates")
-
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    message = "hello from frontend!"
     return templates.TemplateResponse(
         request=request,
         name="chat_page.html",
-        context={"message": message},
+        context={"message": "hello from frontend!"},
     )
